879f21b8b6ff4f11aa0ffb23fa59c573df5ac823
rameshnagaraju <rameshnagaraju@gmail.com>
2019-07-20 11:53:37 +0530

    Initial commit

---
+++ src/main/java/com/example/cris/CrisApplication.java
@@ -0,0 +1,13 @@
+package com.example.cris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisApplication.class, args);
+	}
+
+}
